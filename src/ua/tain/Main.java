8b92bb2efb4f255f8a634cd36211aacacec8934e
Oleg Kharchenko <oleg.kharchenko@tain.com>
2018-10-24 09:27:00 +0300

    frame only

--- src/ua/tain/Main.java
+++ src/ua/tain/Main.java
@@ -7,23 +7,15 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Shall the Game begin");
     Scanner sc = new Scanner(System.in);
-    Game game = new Game();
     boolean keepPlaying = true;
     while (keepPlaying) {
-      game.print();
       String line = sc.nextLine();
       switch (line) {
-        case "u":
-          System.out.println("up");
+        case "cmd1":
+          System.out.println("cmd1");
           break;
-        case "d":
-          game.down();
-          break;
-        case "l":
-          System.out.println("left");
-          break;
-        case "r":
-          game.right();
+        case "cmd2":
+          System.out.println("cmd2");
           break;
         case "q":
           System.out.println("quit");
